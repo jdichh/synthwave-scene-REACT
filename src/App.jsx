cc1c1f462123baedb907794dd9525548be15169e
jdichh <jpsdichoso21@gmail.com>
2023-09-30 22:33:19 +0800

    slowed down anim speed, changed music

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
   const canvasRef = useRef();
   const audio = new Audio("./assets/music/almondcruise.mp3");
   audio.preload = "auto";
+
+  // world peace!
   return (
     <>
       <Three canvasRef={canvasRef} audio={audio} />
